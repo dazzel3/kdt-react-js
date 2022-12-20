7da2d7136f0544d0b6f870b7b300f20e82ff2f89
dazzel3 <aakk9350@naver.com>
2022-12-20 17:54:03 +0900

    feat: create Search function

---
+++ react4/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import EmojiList from './components/EmojiList';
+import Header from './components/Header';
+import SearchBox from './components/SearchBox';
+import emojiJson from './data/emoji.json';
+
+function App() {
+  const [keyword, setKeyword] = useState('');
+
+  return (
+    <div className='App'>
+      <Header />
+      <SearchBox onSearch={setKeyword} />
+      <EmojiList emojis={emojiJson} keyword={keyword} />
+    </div>
+  );
+}
+
+export default App;
